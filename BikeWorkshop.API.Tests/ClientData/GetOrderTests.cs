ca605b64dfe61756fcfa9a574e96054439716f85
TomResz <tomaszres@interia.pl>
2024-03-17 16:04:46 +0100

    Added integration tests of api.

---
+++ BikeWorkshop.API.Tests/ClientData/GetOrderTests.cs
@@ -0,0 +1,41 @@
+﻿using BikeWorkshop.API.Tests.Settings;
+using BikeWorkshop.API.Tests.Settings.BaseClasses;
+using BikeWorkshop.API.Tests.Settings.WebAppFactories;
+using BikeWorkshop.Application.Functions.DTO;
+using FluentAssertions;
+using Newtonsoft.Json;
+
+namespace BikeWorkshop.API.Tests.ClientData;
+public class GetOrderTests 
+    : ManagerBaseClass
+{
+	public GetOrderTests(ManagerTestWebApplicationFactory<Program> factory) : base(factory)
+    {
+	}
+    private async void Init()
+    {
+		await dbContext.Initialize();
+	}
+
+	[Fact]
+    public async Task GetByOrderId_UnknownOrderId_ReturnNotFoundStatus()
+    {
+        var orderId = Guid.NewGuid();
+        var response = await httpClient.GetAsync($"api/ClientData/{Guid.NewGuid}");
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+	public async Task GetByOrderId_ValidOrderId_ReturnsOKStatus()
+	{
+        var orderId = Guid.NewGuid();
+		await dbContext.AddOrderAndClient(orderId, Constants.ManagerId);
+
+		var response = await httpClient.GetAsync($"api/ClientData/{orderId}");
+        var responseContent = JsonConvert.DeserializeObject<ClientDataDto>(await response.Content.ReadAsStringAsync());
+
+		response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        responseContent?.PhoneNumber.Should().NotBeNull();
+	}
+}
---
+++ BikeWorkshop.API.Tests/Settings/WebAppFactories/Common/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using BikeWorkshop.API.Tests.Settings.FakeServices;
+using BikeWorkshop.Application.Interfaces.Services;
+using BikeWorkshop.Infrastructure.EF.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BikeWorkshop.API.Tests.Settings.WebAppFactories.Common;
+public static class ServiceExtensions
+{
+	public static IServiceCollection AddCommonsServices(this IServiceCollection services)
+	{
+		var dbContextOpt = services.SingleOrDefault(
+	s => s.ServiceType == typeof(DbContextOptions<BikeWorkshopDbContext>))!;
+		services.Remove(dbContextOpt);
+
+		var smtpService = services.First(s => s.ServiceType == typeof(ICustomEmailSender));
+		services.Remove(smtpService);
+		services.AddSingleton<ICustomEmailSender, FakeSMTPService>();
+		var db = $"BikeWorkshopDb{Guid.NewGuid()}";
+
+		services.AddDbContext<BikeWorkshopDbContext>(
+			d => d.UseInMemoryDatabase(db));
+		return services;
+	}
+}
